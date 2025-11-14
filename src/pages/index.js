d1f6774f5249f74d76be2de53b09f06306705510
Piyush Rathwa <piyush.rathwa@rishabhsoft.com>
2025-11-14 15:25:16 +0530

    bug fix

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,7 @@
-import React from 'react';
-import { useState } from 'react';
-import { Redirect } from '@docusaurus/router';
 import clsx from 'clsx';
 import Link from '@docusaurus/Link';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 import Layout from '@theme/Layout';
-import HomepageFeatures from '@site/src/components/HomepageFeatures';
-import useLunrSearch from '../components/Hooks/useLunrSearch';
 import Heading from '@theme/Heading';
 import styles from './index.module.css';
 import HomePageSearch from '@site/src/components/HomepageFeatures/HomePageSearch';
